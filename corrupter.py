5e6fb6318dbefd8c7c5df0aa0ecd09e3c2aa0b16
Diego Perez <diepe2892@gmail.com>
2021-04-07 23:55:34 -0600

    When ALLOW_NaN_VALUES is true and a NaN value comes up, it tries again until it gets a non-NaN value (hopefully it never gets hung!).

--- corrupter.py
+++ corrupter.py
@@ -54,8 +54,8 @@ def corrupt_value(val, corruption_prob: float):
             new_binary = change_bit_in_binary(binary, offset)
             new_val = bin_to_float(new_binary)
             if not globals.ALLOW_NaN_VALUES and (math.isnan(new_val) or math.isinf(new_val)):
-                logging.debug("Did not corrupt value at the index because it was a NaN or Inf")
-                return val, False
+                logging.debug("Could not corrupt value at the index because it was a NaN or Inf... trying again")
+                return corrupt_value(val, corruption_prob)
             logging.debug("Location value was corrupted from " + str(val) + " --> " + str(new_val))
             return new_val, True
     logging.debug("Did not corrupt value at the index")
